fdb31770ea8802ae2cd0bb561cd4e1440c4aa772
Jackson May <genericaction1@gmail.com>
2023-04-06 16:03:34 -0700

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// Write your code he
+const main = document.querySelector("#main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Jackson May is the champion";
